a3811083929d1baea809203d4cb1e184331a291d
Al-Amin49 <alamin496832@gmail.com>
2024-09-07 11:36:14 +0600

    refactoring product component

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import useAxiosPublic from "../../components/hooks/useAxiosPublic";
-import { AiOutlineShopping } from "react-icons/ai";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 const Products = () => {
   const axiosPublic = useAxiosPublic();
@@ -45,27 +45,7 @@ const Products = () => {
       {/* prodcuts card */}
 
       <div className="w-3/4 p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto">
-        {filteredProducts.map(product => (
-          <div key={product.chair_name} className="border rounded-lg p-4 bg-white shadow">
-            <img src={product.image} alt={product.chair_name} className="w-full h-48 object-cover mb-4 rounded-md" />
-            <h4 className="text-xl font-semibold mb-2">{product.chair_name}</h4>
-            <p className="text-gray-600 mb-2">
-              <span className="text-2xl font-bold text-secondary">
-                €{product.discounted_price}
-              </span>
-              <span className="text-sm line-through text-gray-400 ml-2">
-                €{product.original_price}
-              </span>
-              <span className="ml-2 text-red-500">30% off</span>
-            </p>
-            <p className="text-gray-700">{product.description}</p>
-          
-            <button className="flex w-full mt-2 bg-secondary items-center justify-center text-white px-2 py-1 rounded">
-            <AiOutlineShopping className="mr-1" />
-              Add to cart
-              </button>
-          </div>
-        ))}
+        {filteredProducts.map(product =><ProductCard key={product._id} product={product}></ProductCard> )}
       </div>
     </div>
   );
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,29 @@
+import { AiOutlineShopping } from "react-icons/ai";
+
+const ProductCard = ({product}) => {
+    return (
+        <>
+          <div key={product.chair_name} className="border rounded-lg p-4 bg-white shadow">
+            <img src={product.image} alt={product.chair_name} className="w-full h-48 object-cover mb-4 rounded-md" />
+            <h4 className="text-xl font-semibold mb-2">{product.chair_name}</h4>
+            <p className="text-gray-600 mb-2">
+              <span className="text-2xl font-bold text-secondary">
+                €{product.discounted_price}
+              </span>
+              <span className="text-sm line-through text-gray-400 ml-2">
+                €{product.original_price}
+              </span>
+              <span className="ml-2 text-red-500">30% off</span>
+            </p>
+            <p className="text-gray-700">{product.description}</p>
+          
+            <button className="flex w-full mt-2 bg-secondary items-center justify-center text-white px-2 py-1 rounded">
+            <AiOutlineShopping className="mr-1" />
+              Add to cart
+              </button>
+          </div>
+        </>
+    );
+};
+
+export default ProductCard;
